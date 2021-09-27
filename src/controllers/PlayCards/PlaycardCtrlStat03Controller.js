66f057039d4bd3aef907dff76ceba1383622a8df
Jorge Noda <jknoda@hotmail.com>
2021-09-27 17:11:21 -0300

    ajustes versus

--- src/controllers/PlayCards/PlaycardCtrlStat03Controller.js
+++ src/controllers/PlayCards/PlaycardCtrlStat03Controller.js
@@ -8,7 +8,7 @@ module.exports = {
         const {ano} = req.body;
         const sql = `
         SELECT 
-            AVG(ST.ptoa) AS ptoa,
+            #AVG(ST.ptoa) AS ptoa,
             (AVG(ST.ala)+1)*1.5 AS ala,
             (AVG(ST.asa)+1)*1.25 AS asa,
             (AVG(ST.cla)+1)*2.5 AS cla,
@@ -16,7 +16,7 @@ module.exports = {
             (AVG(ST.rla)+1)*1.75 AS rla,
             (AVG(ST.rsa)+1)*1.5 AS rsa,
             AVG(ST.jogadasa)+1 AS jogadasa,
-            AVG(ST.ptob) AS ptob,
+            #AVG(ST.ptob) AS ptob,
             (AVG(ST.alb)+1)*1.5 AS alb,
             (AVG(ST.asb)+1)*1.25 AS asb,
             (AVG(ST.clb)+1)*2.5 AS clb,
@@ -52,7 +52,7 @@ module.exports = {
         SELECT * FROM
         (
             SELECT 
-                SUM(ptoa) AS ptoa,
+                #SUM(ptoa) AS ptoa,
                 SUM(asa) AS asa,
                 SUM(ala) AS ala,
                 SUM(csa) AS csa,
@@ -60,7 +60,7 @@ module.exports = {
                 SUM(rsa) AS rsa,
                 SUM(rla) AS rla,
                 SUM(jogadasa) AS jogadasa,
-                SUM(ptob) AS ptob,
+                #SUM(ptob) AS ptob,
                 SUM(asb) AS asb,
                 SUM(alb) AS alb,
                 SUM(csb) AS csb,
@@ -70,7 +70,7 @@ module.exports = {
                 SUM(jogadasb) AS jogadasb
             FROM (
                 SELECT /* JOGADOR A */
-                    AVG(ptoa) AS ptoa,
+                    #AVG(ptoa) AS ptoa,
                     AVG(asa) AS asa,
                     AVG(ala) AS ala,
                     AVG(csa) AS csa,
@@ -78,7 +78,7 @@ module.exports = {
                     AVG(rsa) AS rsa,
                     AVG(rla) AS rla,
                     AVG(jogadasa) AS jogadasa,
-                    AVG(ptob) AS ptob,
+                    #AVG(ptob) AS ptob,
                     AVG(asb) AS asb,
                     AVG(alb) AS alb,
                     AVG(csb) AS csb,
@@ -88,7 +88,7 @@ module.exports = {
                     AVG(jogadasb) AS jogadasb
                 FROM 
                 ((SELECT /* JOGADOR A COL A */ 
-                    AVG(ST.ptoa) AS ptoa,
+                    #AVG(ST.ptoa) AS ptoa,
                     (AVG(ST.ala)+1)*1.5 AS ala,
                     (AVG(ST.asa)+1)*1.25 AS asa,
                     (AVG(ST.cla)+1)*2.5 AS cla,
@@ -96,13 +96,14 @@ module.exports = {
                     (AVG(ST.rla)+1)*1.75 AS rla,
                     (AVG(ST.rsa)+1)*1.5 AS rsa,
                     AVG(ST.jogadasa)+1 AS jogadasa,
-                    0 AS ptob, 0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb
+                    #0 AS ptob, 
+                    0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.ava01 = ${dupla01a} OR CT.ava02 = ${dupla01a}))
                 UNION
                 (SELECT /* JOGADOR A COL B */
-                    AVG(ST.ptob) AS ptoa,
+                    #AVG(ST.ptob) AS ptoa,
                     (AVG(ST.alb)+1)*1.5 AS ala,
                     (AVG(ST.asb)+1)*1.25 AS asa,
                     (AVG(ST.clb)+1)*2.5 AS cla,
@@ -110,7 +111,8 @@ module.exports = {
                     (AVG(ST.rlb)+1)*1.75 AS rla,
                     (AVG(ST.rsb)+1)*1.5 AS rsa,
                     AVG(ST.jogadasb)+1 AS jogadasa,
-                    0 AS ptob, 0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb
+                    #0 AS ptob, 
+                    0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.avb01 = ${dupla01a} OR CT.avb02 = ${dupla01a}))
@@ -119,7 +121,7 @@ module.exports = {
                 UNION
                 
                 SELECT /* JOGADOR B */
-                    AVG(ptoa) AS ptoa,
+                    #AVG(ptoa) AS ptoa,
                     AVG(asa) AS asa,
                     AVG(ala) AS ala,
                     AVG(csa) AS csa,
@@ -127,7 +129,7 @@ module.exports = {
                     AVG(rsa) AS rsa,
                     AVG(rla) AS rla,
                     AVG(jogadasa) AS jogadasa,
-                    AVG(ptob) AS ptob,
+                    #AVG(ptob) AS ptob,
                     AVG(asb) AS asb,
                     AVG(alb) AS alb,
                     AVG(csb) AS csb,
@@ -137,7 +139,7 @@ module.exports = {
                     AVG(jogadasb) AS jogadasb
                 FROM 
                 ((SELECT /* JOGADOR B COL A */
-                    AVG(ST.ptoa) AS ptoa,
+                    #AVG(ST.ptoa) AS ptoa,
                     (AVG(ST.ala)+1)*1.5 AS ala,
                     (AVG(ST.asa)+1)*1.25 AS asa,
                     (AVG(ST.cla)+1)*2.5 AS cla,
@@ -145,13 +147,14 @@ module.exports = {
                     (AVG(ST.rla)+1)*1.75 AS rla,
                     (AVG(ST.rsa)+1)*1.5 AS rsa,
                     AVG(ST.jogadasa)+1 AS jogadasa,
-                    0 AS ptob, 0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb
+                    #0 AS ptob, 
+                    0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.ava01 = ${dupla01b} OR CT.ava02 = ${dupla01b})) 
                 UNION
                 (SELECT /* JOGADOR B COL B */
-                    AVG(ST.ptob) AS ptoa,
+                    #AVG(ST.ptob) AS ptoa,
                     (AVG(ST.alb)+1)*1.5 AS ala,
                     (AVG(ST.asb)+1)*1.25 AS asa,
                     (AVG(ST.clb)+1)*2.5 AS cla,
@@ -159,7 +162,8 @@ module.exports = {
                     (AVG(ST.rlb)+1)*1.75 AS rla,
                     (AVG(ST.rsb)+1)*1.5 AS rsa,
                     AVG(ST.jogadasb)+1 AS jogadasa,
-                    0 AS ptob, 0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb    
+                    #0 AS ptob, 
+                    0 AS alb, 0 AS asb, 0 AS clb, 0 AS csb, 0 AS rlb, 0 AS rsb, 0 AS jogadasb    
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.avb01 = ${dupla01b} OR CT.avb02 = ${dupla01b}))
@@ -168,7 +172,7 @@ module.exports = {
                 UNION /* UNIR COM ADVERSÁRIOS */
                 
                 SELECT /* ADVERSARIO A */
-                    AVG(ptoa) AS ptoa,
+                    #AVG(ptoa) AS ptoa,
                     AVG(asa) AS asa,
                     AVG(ala) AS ala,
                     AVG(csa) AS csa,
@@ -176,7 +180,7 @@ module.exports = {
                     AVG(rsa) AS rsa,
                     AVG(rla) AS rla,
                     AVG(jogadasa) AS jogadasa,
-                    AVG(ptob) AS ptob,
+                    #AVG(ptob) AS ptob,
                     AVG(asb) AS asb,
                     AVG(alb) AS alb,
                     AVG(csb) AS csb,
@@ -186,7 +190,7 @@ module.exports = {
                     AVG(jogadasb) AS jogadasb
                 FROM 
                 ((SELECT /* ADVERSARIO A COL A */
-                    AVG(ST.ptoa) AS ptob,
+                    #AVG(ST.ptoa) AS ptob,
                     (AVG(ST.ala)+1)*1.5 AS alb,
                     (AVG(ST.asa)+1)*1.25 AS asb,
                     (AVG(ST.cla)+1)*2.5 AS clb,
@@ -194,13 +198,14 @@ module.exports = {
                     (AVG(ST.rla)+1)*1.75 AS rlb,
                     (AVG(ST.rsa)+1)*1.5 AS rsb,
                     AVG(ST.jogadasa)+1 AS jogadasb,
-                    0 AS ptoa, 0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
+                    #0 AS ptoa, 
+                    0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.ava01 = ${dupla02a} OR CT.ava02 = ${dupla02a}))
                 UNION
                 (SELECT /* ADVERSARIO A COL B */
-                    AVG(ST.ptob) AS ptob,
+                    #AVG(ST.ptob) AS ptob,
                     (AVG(ST.alb)+1)*1.5 AS alb,
                     (AVG(ST.asb)+1)*1.25 AS asb,
                     (AVG(ST.clb)+1)*2.5 AS clb,
@@ -208,7 +213,8 @@ module.exports = {
                     (AVG(ST.rlb)+1)*1.75 AS rlb,
                     (AVG(ST.rsb)+1)*1.5 AS rsb,
                     AVG(ST.jogadasb)+1 AS jogadasb,
-                    0 AS ptoa, 0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
+                    #0 AS ptoa, 
+                    0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.avb01 = ${dupla02a} OR CT.avb02 = ${dupla02a}))
@@ -217,7 +223,7 @@ module.exports = {
                 UNION
                 
                 SELECT /* ADVERSARIO B */
-                    AVG(ptoa) AS ptoa,
+                    #AVG(ptoa) AS ptoa,
                     AVG(asa) AS asa,
                     AVG(ala) AS ala,
                     AVG(csa) AS csa,
@@ -225,7 +231,7 @@ module.exports = {
                     AVG(rsa) AS rsa,
                     AVG(rla) AS rla,
                     AVG(jogadasa) AS jogadasa,
-                    AVG(ptob) AS ptob,
+                    #AVG(ptob) AS ptob,
                     AVG(asb) AS asb,
                     AVG(alb) AS alb,
                     AVG(csb) AS csb,
@@ -235,7 +241,7 @@ module.exports = {
                     AVG(jogadasb) AS jogadasb
                 FROM 
                 ((SELECT  /* ADVERSARIO B COL A */
-                    AVG(ST.ptoa) AS ptob,
+                    #AVG(ST.ptoa) AS ptob,
                     (AVG(ST.ala)+1)*1.5 AS alb,
                     (AVG(ST.asa)+1)*1.25 AS asb,
                     (AVG(ST.cla)+1)*2.5 AS clb,
@@ -243,13 +249,14 @@ module.exports = {
                     (AVG(ST.rla)+1)*1.75 AS rlb,
                     (AVG(ST.rsa)+1)*1.5 AS rsb,
                     AVG(ST.jogadasa)+1 AS jogadasb,
-                    0 AS ptoa, 0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
+                    #0 AS ptoa, 
+                    0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.ava01 = ${dupla02b} OR CT.ava02 = ${dupla02b})) 
                 UNION
                 (SELECT /* ADVERSARIO B COL B */
-                    AVG(ST.ptob) AS ptob,
+                    #AVG(ST.ptob) AS ptob,
                     (AVG(ST.alb)+1)*1.5 AS alb,
                     (AVG(ST.asb)+1)*1.25 AS asb,
                     (AVG(ST.clb)+1)*2.5 AS clb,
@@ -257,7 +264,8 @@ module.exports = {
                     (AVG(ST.rlb)+1)*1.75 AS rlb,
                     (AVG(ST.rsb)+1)*1.5 AS rsb,
                     AVG(ST.jogadasb)+1 AS jogadasb,
-                    0 AS ptoa, 0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
+                    #0 AS ptoa, 
+                    0 AS ala, 0 AS asa, 0 AS cla, 0 AS csa, 0 AS rla, 0 AS rsa, 0 AS jogadasa
                 FROM playcardctrlstat ST
                 INNER JOIN playcardctrl CT ON CT.idf = ST.idf
                 WHERE CT.inicial > 0 AND (CT.avb01 = ${dupla02b} OR CT.avb02 = ${dupla02b}))
